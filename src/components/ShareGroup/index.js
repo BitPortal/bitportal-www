17f87de1c87f910cee4be9c3b4e19dbc9b8c9294
litianao <exrobbie@gmail.com>
2018-07-09 20:22:55 +0800

    optimized style

--- src/components/ShareGroup/index.js
+++ src/components/ShareGroup/index.js
@@ -6,42 +6,55 @@ class ShareGroup extends React.Component {
         let socialLinkTheme = this.props.theme
         let socialLinkClass = ''
         if (socialLinkTheme === 'gradient') {
-            socialLinkClass = 'social-link social-link-gradient'
+            socialLinkClass = 'row social-link social-link-gradient'
         } else {
-            socialLinkClass = 'social-link social-link-dark'
+            socialLinkClass = 'row social-link social-link-dark'
         }
         return (
             <div className={socialLinkClass}>
-                <div className="social-link-item">
-                    <a href="https://github.com/BitPortal">
-                        <span className="icon icon-github"></span>
-                        <span className="social-link-text">Github</span>
-                    </a>
-                </div>
-                <div className="social-link-item">
-                    <a href="https://t.me/BitPortal_official_EN">
-                        <span className="icon icon-telegram"></span>
-                        <span className="social-link-text">Telegram</span>
-                    </a>
-                </div>
-                <div className="social-link-item">
-                    <a href="#">
-                        <span className="icon icon-wechat"></span>
-                        <span className="social-link-text">Wechat</span>
-                    </a>
-                </div>
-                <div className="social-link-item">
-                    <a href="https://discordapp.com/invite/ZRUvfxV">
-                        <span className="icon icon-discord"></span>
-                        <span className="social-link-text">Discord</span>
-                    </a>
-                </div>
-                <div className="social-link-item">
-                    <a href="https://twitter.com/BitPortal_IO">
-                        <span className="icon icon-twitter"></span>
-                        <span className="social-link-text">Twitter</span>
-                    </a>
-                </div>
+                {/*<div className="col-md">*/}
+                    <div className="col-xs-6 col-sm-4 col-md-2">
+                        <div className="social-link-item">
+                            <a href="https://github.com/BitPortal">
+                                <span className="icon icon-github"></span>
+                                <span className="social-link-text">Github</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-4 col-md-2">
+                        <div className="social-link-item">
+                            <a href="https://t.me/BitPortal_official_EN">
+                                <span className="icon icon-telegram"></span>
+                                <span className="social-link-text">Telegram</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-4 col-md-2">
+                        <div className="social-link-item">
+                            <a href="#">
+                                <span className="icon icon-wechat"></span>
+                                <span className="social-link-text">Wechat</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-4 col-md-2">
+                        <div className="social-link-item">
+                            <a href="https://discordapp.com/invite/ZRUvfxV">
+                                <span className="icon icon-discord"></span>
+                                <span className="social-link-text">Discord</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-4 col-md-2">
+                        <div className="social-link-item">
+                            <a href="https://twitter.com/BitPortal_IO">
+                                <span className="icon icon-twitter"></span>
+                                <span className="social-link-text">Twitter</span>
+                            </a>
+                        </div>
+                    </div>
+                {/*</div>*/}
+
             </div>
         )
     }
--- src/pages/article.js
+++ src/pages/article.js
@@ -20,7 +20,7 @@ class ArticlePage extends React.Component {
                 <SectionBanner/>
                 <div className="container">
                     <div className="bread-crumb">
-                        <a href="http://www.corp.bitportal.io"><FormattedMessage id="nav.home"/></a> - <a href="http://35.194.142.236:8000/5b2a5fc57c74855560155ac8/"><FormattedMessage id="nav.blog"/></a> - <span className="bread-crumb-title">{this.data.strapiArticle.title}</span>
+                        <a href="http://www.corp.bitportal.io" className="bread-crumb-link"><FormattedMessage id="nav.home"/></a> - <a href="http://35.194.142.236:8000/5b2a5fc57c74855560155ac8/" className="bread-crumb-link"><FormattedMessage id="nav.blog"/></a> - <span className="bread-crumb-title">{this.data.strapiArticle.title}</span>
                     </div>
                     <div className="article-page-wrap">
                         <div className="row">
@@ -42,11 +42,17 @@ class ArticlePage extends React.Component {
                                 <div className="article-content">
                                     <Markdown source={this.data.strapiArticle.content}/>
                                 </div>
+
+                            </div>
+                        </div>
+                        <div className="row article-share-group">
+                            <div className="col-sm-10 col-sm-offset-2">
+                                <div className="article-share-hint">分享到</div>
                                 <ShareGroup theme={'gradient'}/>
                             </div>
                         </div>
                         <div className="article-divider"> </div>
-                        /* Hide ArticleComment Temporarily */
+                        {/* Hide Article Comment temporarily */}
                         {/*<div className="row">*/}
                             {/*<div className="col-sm-10 col-sm-offset-2">*/}
                                 {/*<ArticleComment/>*/}
