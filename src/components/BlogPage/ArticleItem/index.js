5dc89c5b1cc91500a2ff44c5334e8e4b5ff6f2b3
litianao <exrobbie@gmail.com>
2018-07-12 08:51:29 +0800

    removed row

--- src/components/BlogPage/ArticleItem/index.js
+++ src/components/BlogPage/ArticleItem/index.js
@@ -2,17 +2,18 @@ import React from 'react'
 import './style.less'
 import {FormattedMessage} from 'react-intl'
 
-class ArticleItem extends React.Component{
-    componentWillMount () {
+class ArticleItem extends React.Component {
+    componentWillMount() {
         //format brief article content
         let articleContent = this.props.articleData.node.content
         this.props.articleData.node.content = articleContent.replace(/[#]/g, '').slice(0, 200)
     }
-    render () {
+
+    render() {
         //conditionally render article image and tags
         let articleImg = '',
             articleTag = '';
-        if (this.props.articleData.node.img_url){
+        if (this.props.articleData.node.img_url) {
             articleImg = <img className="article-img" src={this.props.articleData.node.img_url}/>
         }
         if (this.props.articleData.node.tag) {
@@ -20,22 +21,20 @@ class ArticleItem extends React.Component{
         }
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-sm-3">
-                        <div className="article-item">
-                            {articleImg}
-                            <div className="article-item-padding-zone">
-                                {articleTag}
-                                <div className="article-title">{this.props.articleData.node.title}</div>
-                                <div className="article-content">
-                                    {this.props.articleData.node.content}
-                                </div>
-                                <a href={`/blog/${this.props.articleData.node.id}`}>
-                                    <button className="article-btn"><FormattedMessage id="viewMore"/></button>
-                                </a>
+                <div className="col-sm-3">
+                    <div className="article-item">
+                        {articleImg}
+                        <div className="article-item-padding-zone">
+                            {articleTag}
+                            <div className="article-title">{this.props.articleData.node.title}</div>
+                            <div className="article-content">
+                                {this.props.articleData.node.content}
                             </div>
-                            <div className="article-shadow"> </div>
+                            <a href={`/blog/${this.props.articleData.node.id}`}>
+                                <button className="article-btn"><FormattedMessage id="viewMore"/></button>
+                            </a>
                         </div>
+                        <div className="article-shadow"></div>
                     </div>
                 </div>
             </div>
--- src/components/BlogPage/BannerItem/index.js
+++ src/components/BlogPage/BannerItem/index.js
@@ -2,16 +2,14 @@ import React from 'react'
 import './style.less'
 
 class BannerItem extends React.Component {
-    render () {
+    render() {
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-sm-6">
-                        <div className="banner-item">
-                            <a href={this.props.bannerData.node.jump_url}>
-                                <img className="banner-img" src={this.props.bannerData.node.img_url}/>
-                            </a>
-                        </div>
+                <div className="col-sm-6">
+                    <div className="banner-item">
+                        <a href={this.props.bannerData.node.jump_url}>
+                            <img className="banner-img" src={this.props.bannerData.node.img_url}/>
+                        </a>
                     </div>
                 </div>
             </div>
