4c8a1057111d46c3d99f677ee29ba5c71ff51068
litianao <exrobbie@gmail.com>
2018-07-25 12:07:09 +0800

    get intl 404 back

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,22 @@
 import React from 'react'
-
+import Layout from '../components/Layout'
+import {withIntl} from '../i18n'
 import {FormattedMessage} from 'react-intl'
 import '../styles/pages/404.less'
 
-class NotFoundPage extends React.Component {
-    constructor({props}) {
-        super(props)
-    }
-
-    render() {
+class NotFoundPage extends React.Component{
+    render () {
         return (
+            <Layout>
                 <div className="not-found-page">
-                    <h1><FormattedMessage id="notFound.title"/></h1>
-                    <p><FormattedMessage id="notFound.content"/></p>
+                    <div className="container">
+                        <h1><FormattedMessage id="notFound.title"/></h1>
+                        <p><FormattedMessage id="notFound.content"/></p>
+                    </div>
                 </div>
+            </Layout>
         )
     }
 }
 
-export default NotFoundPage
+export default withIntl(NotFoundPage)
