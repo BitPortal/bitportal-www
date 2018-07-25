6559be7cd11baaae4b0ffb899afc6e61ce7132f0
litianao <exrobbie@gmail.com>
2018-07-25 11:58:37 +0800

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import Layout from '../components/Layout'
 
 import {FormattedMessage} from 'react-intl'
 import '../styles/pages/404.less'
 
 class NotFoundPage extends React.Component {
-    constructor({props, data}) {
+    constructor({props}) {
         super(props)
-        this.data = data
     }
 
     render() {
         return (
-            <Layout>
                 <div className="not-found-page">
                     <h1><FormattedMessage id="notFound.title"/></h1>
                     <p><FormattedMessage id="notFound.content"/></p>
                 </div>
-            </Layout>
         )
     }
 }
